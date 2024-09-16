2944452e5f6e29c5a006550977dca23da9400093
JulianArchuleta <troyarchuleta@mail.weber.edu>
2024-09-16 09:48:43 -0600

    get_data() working

--- data/get_data.py
+++ data/get_data.py
@@ -1,34 +1,61 @@
 """Download data from our server
 """
+import zipfile
+import os
 import requests
-import zipp
-SERVER_URL = 'http://icarus.cs.weber.edu/~hvalle/cs4580/data/'
+from io import BytesIO
+SERVER_URL = "http://icarus.cs.weber.edu/~hvalle/cs4580/data/"
+FILE_NAME = "pandas02Data.zip"
+
 
 def download_file(url, file_name):
+    """Download a file from the server and save it to pwd
+    
+    Parameters
+    ----------
+    url : str
+        The URL of the file to download
+    file_name : str
+        The name of the file to save it to
+    """
     # TODO: Download to pwd
-    file = requests.get(url)   
-    print(f'Error: {file.status_code}')
-    # TODO: Check extension, if it zip call unzip_file
-    if file_name.endswith('.zip'):
-        unzip_file(file)
-    pass
+    url = url + file_name
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
+        print(f'Downloaded {file_name}')
+        if zipfile.is_zipfile(file_name):
+            unzip_file(file_name)
+    else:
+        print(f'Download failed: {response.status_code}')
+    
 
 
 def unzip_file(file_name):
-    # TODO: Unzip file
-    with zipp.zipfile.ZipFile(file_name) as file:
-        file.extractall()
-    pass
-
-
-def main():
-    """Driven Function
-    """
-    data01 = 'pandas01Data.zip'
-    file = download_file(SERVER_URL,  data01)
+    """Unzip a file in the current working directory
     
+    Parameters
+    ----------
+    file_name : str
+        Name of the file to be unzipped
+    """
+    # TODO: Unzip file    
+    with zipfile.ZipFile(file_name, "r") as zip_ref:
+        zip_ref.extractall()
+        print(f'Unzipped {file_name}')
+    os.remove(file_name)
+    
+    
+
+
+def main(server_url, file_name):
+    """Driven Function
+    """    
+    download_file(server_url,  file_name)
     
     
 
 if(__name__ == '__main__'):
-    main()
+    main(SERVER_URL, FILE_NAME)
