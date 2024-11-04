3688601c02009fe8e0bb0308163cd72ae8d88b44
JulianArchuleta <troyarchuleta@mail.weber.edu>
2024-11-04 09:20:31 -0700

    midclass push

--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -2,16 +2,36 @@
 """
 import pandas as pd
 import numpy as np
-import get_data as gt # your package
+import get_data as gt  # your package
 
 # Constants
-K = 10 # number of closest matches
-BASE_CASE_ID = 88763 # IMDB_id for 'Back to the Future'
+K = 10  # number of closest matches
+BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
 
 
-def knn_analysis_driver(df, base_case, comparison_type, metric_stub, sorted_value='metric'):
+def metric_stub(base_case_value, comparator_value):
+    return 0
+
+
+def print_top_k(df, sorted_value, comparison_type):
+    print(f'Top {K} closest matches to {comparison_type}')
+    counter = 1
+    for idx, row in df.head(K).iterrows():
+        print(f'Top {counter} match:[{idx}]:{row["year"]}, {
+              row["title"]},{row["genres"]},  {row[sorted_value]}')
+        counter += 1
+
+
+def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
+    df = data_df.copy()
     # WIP: Create df of filter data
-    df[sorted_value] = df[comparison_type].map(lambda x: metric_stub(base_case[comparison_type], x))
+    df[sorted_value] = df[comparison_type].map(
+        lambda x: metric_func(base_case[comparison_type], x))
+    # Sort return values from functio, stub
+    sorted_df = df.sort_values(by=sorted_value)
+    sorted_df = sorted_df.drop(BASE_CASE_ID)
+    # Print top 10 values
+    print_top_k(sorted_df, sorted_value, comparison_type)
 
 
 def main():
@@ -26,8 +46,12 @@ def main():
     print(f'Loaded {len(data)} records')
     print(f'Data set Columns {data.columns}')
     print(f'Data set description {data.describe()}')
-    # TODO: The rest of your code goes here
-
+    # TASK 3: KNN Analysis
+    print(f'Task 3: KNN Analysis')
+    base_case = data.loc[BASE_CASE_ID]
+    print(f'Comparing all movies to our base case: {base_case["title"]}')
+    knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
+                        metric_func=metric_stub, sorted_value='metric')
 
 
 if __name__ == '__main__':
