609baa6810b2a56803b9cb9f130e35669a246b1a
JulianArchuleta <troyarchuleta@mail.weber.edu>
2024-11-04 10:11:07 -0700

    code push

--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -7,6 +7,7 @@ import get_data as gt  # your package
 # Constants
 K = 10  # number of closest matches
 BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
+BASE_YEAR = 1985  # year for 'Back to the Future'
 
 
 def metric_stub(base_case_value, comparator_value):
@@ -20,11 +21,18 @@ def print_top_k(df, sorted_value, comparison_type):
         print(f'Top {counter} match:[{idx}]:{row["year"]}, {
               row["title"]}, {row["genres"]}, [{row[sorted_value]}]')
         counter += 1
-        
-        
+
+
 def euclidean_distance(base_case_year: int, comparator_year: int):
     return abs(base_case_year - comparator_year)
 
+
+def jaccard_similarity_normal(base_case_genres: str, comparator_genres: str):
+    base_case_set = set(base_case_genres.split(';'))
+    comparator_set = set(comparator_genres.split(';'))
+    intersection = len(base_case_set.intersection(comparator_set))
+    union = len(base_case_set.union(comparator_set))
+    return float(intersection / union)
 
 
 def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
@@ -33,8 +41,11 @@ def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted
     df[sorted_value] = df[comparison_type].map(
         lambda x: metric_func(base_case[comparison_type], x))
     # Sort return values from functio, stub
-    sorted_df = df.sort_values(by=sorted_value)
-    sorted_df = sorted_df.drop(BASE_CASE_ID)
+    if 'jaccard' in metric_func.__name__:
+        sorted_df = df.sort_values(by=sorted_value, ascending=False)
+    else:
+        sorted_df = df.sort_values(by=sorted_value) # default is ascending
+    sorted_df = sorted_df.drop(BASE_CASE_ID) # drop base case
     # Print top 10 values
     print_top_k(sorted_df, sorted_value, comparison_type)
 
@@ -60,7 +71,12 @@ def main():
     # TASK 4: KNN Analysis with Euclidean Distance
     print(f'Task 4: KNN Analysis with Euclidean Distance')
     knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='year',
-                        metric_func=euclidean_distance, sorted_value='metric')
+                        metric_func=euclidean_distance, sorted_value='euclidean_distance')
+    # TASK 5: Jaccard Similarity
+    print(f'Task 5: KNN Analysis with Jaccard Similarity')
+    data = data[data['year'] >= BASE_YEAR]  # added filter
+    knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
+                        metric_func=jaccard_similarity_normal, sorted_value='jaccard_similarity')
 
 
 if __name__ == '__main__':
