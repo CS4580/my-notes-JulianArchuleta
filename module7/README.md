0d7c81f11136b0e1ece85026f094b64da679aa2e
JulianArchuleta <troyarchuleta@mail.weber.edu>
2024-11-11 08:58:29 -0700

    updated read me

--- module7/README.md
+++ module7/README.md
@@ -1,30 +1,29 @@
 # Module 7 Notes: Metrics and Model Development
 
-
 ## Metrics
 
 Metrics should be `unbiased`, universal, and concise.
-    
+
 1. A way to obtain similar responses
-2. A way to measure the performance 
-3. A way to measure prediction. 
+2. A way to measure the performance
+3. A way to measure prediction.
 
 For our sample analysis we will use `KNN` K-Nearest Neighbor
+
 - `K` is an arbitrary pick
 - Need a `base case`
 - Compare the neighbors
 - Sor the results
 
-Dataset for this analysis: 
+Dataset for this analysis:
+
 ```bash
 icarus.cs.weber.edu:~hvalle/cs4580/data/movies.csv
 ```
 
-
 ### KNN-Euclidean Distance
 
 Note: All code will be in `knn_analysis.py`
-
 
 The Euclidean distance is the distance between points in `N-dimensional` space.
 
@@ -33,11 +32,14 @@ $
 d(p, q) = \sqrt{\sum_{i=1}^n (q_i - p_i)^2}
 $
 where:
+
 - $p = (p_1, p_2, \dots, p_n)$
 - $q = (q_1, q_2, \dots, q_n)$
 
-#### Task: 
+#### Task:
+
 Find the distance between these points:
+
 - x = (0,0)
 - y = (4,4)
 
@@ -49,7 +51,8 @@ def euclidean_distance()
 ```
 
 ### KNN with Jaccard Similarity Index
-Compares members of two individual sets to determine which members are `shared` and which are `distinct`. The index measures the similarity between the two sets. 
+
+Compares members of two individual sets to determine which members are `shared` and which are `distinct`. The index measures the similarity between the two sets.
 
 $$
 J(A, B) = \frac{|A \cap B|}{|A \cup B|}
@@ -63,10 +66,12 @@ def jaccard_similarity_normal()
 ```
 
 ### KNN with Weighted Jaccard Similarity Index
-The traditional Jaccard works well when doing `one-to-one` comparisons between a category. It does not work on multiple categories. 
 
-One solution is the `weighted` version. 
-- Build a dictionary for `each genre` of the movies in our preferred list 
+The traditional Jaccard works well when doing `one-to-one` comparisons between a category. It does not work on multiple categories.
+
+One solution is the `weighted` version.
+
+- Build a dictionary for `each genre` of the movies in our preferred list
 
 ```python
 # see
@@ -74,26 +79,30 @@ def jaccard_similarity_weighted()
 ```
 
 ### KNN with Levenshtein Distance
-This is the most common form of edit-based metric, which generally quantifies to work required to transform a string from an initial sequence to a target sequence. 
+
+This is the most common form of edit-based metric, which generally quantifies to work required to transform a string from an initial sequence to a target sequence.
+
 - It is used to determine the difference between two sequences (strings)
-- It is the distance between two words (minimum number of digits edits)
-    - insertions, deletions, or substitutions
-$$
-D(i, j) = 
-\begin{cases}
-j & \text{if } i = 0 \\
-i & \text{if } j = 0 \\
-D(i-1, j-1) & \text{if } s[i] = t[j] \\
-1 + \min \{D(i-1, j), D(i, j-1), D(i-1, j-1)\} & \text{if } s[i] \neq t[j]
-\end{cases}
-$$
+- It is the distance between two words (minimum number of digits edits) - insertions, deletions, or substitutions
+  $$
+  D(i, j) =
+  \begin{cases}
+  j & \text{if } i = 0 \\
+  i & \text{if } j = 0 \\
+  D(i-1, j-1) & \text{if } s[i] = t[j] \\
+  1 + \min \{D(i-1, j), D(i, j-1), D(i-1, j-1)\} & \text{if } s[i] \neq t[j]
+  \end{cases}
+  $$
 
 #### For example:
+
 Consider these strings:
+
 - s = 'kitten'
 - t = 'sitting'
 
 Find the `Levenshteain` Distance
+
 1. Substitude `k` with `s` in `kitten` -> `sitten` (1 substitution)
 2. Substitude `e` with `i` in `sitten` -> `sittin` (1 substitution)
 3. Insert `g` at the end of `sittin` -> `sitting` (1 insertion)
@@ -104,20 +113,49 @@ Result is 3 edits, so the distances is $ = 3$
 # see
 def knn_levenshtein_title()
 ```
+
 Need this package
+
 ```bash
 # VE must be running python 3.11 or less
 pip install Levenshtein
 ```
 
 ### KNN Cosine Similarity Distance
-It is used to measure the cosine of the angle between two vectors in a multi-dimensional space. This is commonly use in text analysis to measure similarities between documents. 
+
+It is used to measure the cosine of the angle between two vectors in a multi-dimensional space. This is commonly use in text analysis to measure similarities between documents.
 
 $$
-\text{Cosine Similarity} = \cos(\theta)  
-= \frac{A \cdot B}{|A| |B|} 
+\text{Cosine Similarity} = \cos(\theta)
+= \frac{A \cdot B}{|A| |B|}
 = \frac{\sum_{i=1}^{n} A_i B_i} { \sqrt{sum_{i=1}^n} A_i^2 \cdot \sqrt{\sum_{i=1}^{n} B_i^2}}
 $$
-where 
+
+where
+
 - $ A \cdot B$ is the dot product of vectors $A$ and $B$
 - $|A|$ and $|B|$ are the magnitude (or Euclidean norms) of vectors $A$ and $B$
+
+### KNN Combining Metrics and Filtering Condiitions
+
+Two main concerns with `filtering`:
+
+- Make too complicated (hard SQL queries)
+- Too strict (end up with no results)
+
+Combine ` metrics` to generate `one` results:
+
+- Weight each metric
+  - Should metric contribute equally (50%-50%, 80%-20%)
+- Normalization of the combine metric
+  - Make sure they have the same range
+
+For our example, we will use:
+
+- `Cosine`: Use 20% of the `plot`
+- `Weighted Jaccard`: Use 80% of `genres`
+
+```python
+# See
+cosine_and_weighted_jaccard()
+```
--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -3,14 +3,29 @@
 import pandas as pd
 import numpy as np
 import get_data as gt  # your package
+import Levenshtein  # Levenshtein distance
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Constants
 K = 10  # number of closest matches
 BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
+SECOND_CASE_ID = 89530  # IMDB_id for 'Mad Max Beyond Thunderdome'
 BASE_YEAR = 1985  # year for 'Back to the Future'
+
+METRIC1_WT = 0.2  # weight for cosine similarity
+METRIC2_WT = 0.8  # weight for weighted Jaccard similarity
 
 
 def metric_stub(base_case_value, comparator_value):
+    """
+    Dummy metric function
+    base_case_value (any type): The base value to compare against.
+    comparator_value (any type): The value to compare with the base case.
+    Returns:
+    int: Always returns 0 as this is a stub function.
+    """
+
     return 0
 
 
@@ -35,6 +50,70 @@ def jaccard_similarity_normal(base_case_genres: str, comparator_genres: str):
     return float(intersection / union)
 
 
+def _get_weighted_jaccard_similarity_dict(df):
+    # put our selections of 'Back to the Future'(88763) and 'Mad Max Beyond Thunderome'(89530) into a list:
+    selections_df = [df.loc[BASE_CASE_ID], df.loc[SECOND_CASE_ID]]
+
+    # genres_weighted_dictionary is needed for the weighted Jaccard similarity index:
+    genres_weighted_dictionary = {"total": 0}
+    for movie in selections_df:
+        # the genres are separated by a semicolon
+        for genre in movie["genres"].split(";"):
+            if genre in genres_weighted_dictionary:
+                genres_weighted_dictionary[genre] += 1
+            else:
+                genres_weighted_dictionary[genre] = 1
+            genres_weighted_dictionary["total"] += 1
+
+    # print(f"\t**genres_weighted_dictionary = {genres_weighted_dictionary}")
+    return genres_weighted_dictionary
+
+
+def weighted_jaccard_similarity(df: pd.DataFrame, comparator_genres: str):
+    # weighted_dictionary is based on all the selections that the user has made so far
+    # comparator_genres is another movie's genres that is being compared
+    weighted_dictionary = _get_weighted_jaccard_similarity_dict(df)
+    numerator = 0
+    denominator = weighted_dictionary["total"]
+    for genre in comparator_genres.split(";"):
+        if genre in weighted_dictionary:
+            numerator += weighted_dictionary[genre]
+
+    return numerator / denominator
+
+
+def cosine_similarity_function(base_case_plot, comparator_plot):
+    # this line will convert the plots from strings to vectors in a single matrix:
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(
+        (base_case_plot, comparator_plot))
+    results = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])
+    return results[0][0]
+
+
+def cosine_and_weighted_jaccard(df: pd.DataFrame, plots: str, comparator_movie: pd.core.series.Series,):
+    # Perform the cosine similiarty and weighted Jaccard metrics:
+    cs_result = cosine_similarity_function(plots, comparator_movie["plot"])
+    weighted_dictionary = _get_weighted_jaccard_similarity_dict(df)
+    wjs_result = weighted_jaccard_similarity(
+        df, comparator_movie["genres"]
+    )
+
+    # Normalization:
+    # The weighted Jaccard similarity result has a range from 0.0 to 1.0.
+    # The cosine similarity result has a range from -1.0 to 1.0. We need to change the range for the cosine similarity result.
+    # First, add 1 to the cosine similarity result so that it has a range from 0.0 to 2.0
+    # Second, divide the result by 2.0 so that it has a range from 0.0 to 1.0:
+    cs_result = (cs_result + 1) / 2.0
+
+    # Weights:
+    # Use a weight of 0.2 (20%) for the cosine similarity result:
+    cs_result *= METRIC1_WT
+    # Use a weight of 0.8 (80%) for the weighted Jaccard similarity result:
+    wjs_result *= METRIC2_WT
+    return wjs_result + cs_result
+
+
 def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
     df = data_df.copy()
     # WIP: Create df of filter data
@@ -44,8 +123,8 @@ def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted
     if 'jaccard' in metric_func.__name__:
         sorted_df = df.sort_values(by=sorted_value, ascending=False)
     else:
-        sorted_df = df.sort_values(by=sorted_value) # default is ascending
-    sorted_df = sorted_df.drop(BASE_CASE_ID) # drop base case
+        sorted_df = df.sort_values(by=sorted_value)  # default is ascending
+    sorted_df = sorted_df.drop(BASE_CASE_ID)  # drop base case
     # Print top 10 values
     print_top_k(sorted_df, sorted_value, comparison_type)
 
@@ -55,28 +134,78 @@ def main():
     print(f'Task 1: Download dataset from server')
     dataset_file = 'movies.csv'
     gt.download_dataset(gt.ICARUS_CS4580_DATASET_URL, dataset_file)
+
     # TASK 2: Load  data_file into a DataFrame
-    print(f'Task 2: Load movie data into a DataFrame')
+    print(f'\nTask 2: Load movie data into a DataFrame')
     data_file = f'{gt.DATA_FOLDER}/{dataset_file}'
     data = gt.load_data(data_file, index_col='IMDB_id')
     print(f'Loaded {len(data)} records')
     print(f'Data set Columns {data.columns}')
     print(f'Data set description {data.describe()}')
+
     # TASK 3: KNN Analysis
-    print(f'Task 3: KNN Analysis')
+    print(f'\nTask 3: KNN Analysis')
     base_case = data.loc[BASE_CASE_ID]
     print(f'Comparing all movies to our base case: {base_case["title"]}')
     knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
                         metric_func=metric_stub, sorted_value='metric')
+
     # TASK 4: KNN Analysis with Euclidean Distance
-    print(f'Task 4: KNN Analysis with Euclidean Distance')
+    print(f'\nTask 4: KNN Analysis with Euclidean Distance')
     knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='year',
                         metric_func=euclidean_distance, sorted_value='euclidean_distance')
+
     # TASK 5: Jaccard Similarity
-    print(f'Task 5: KNN Analysis with Jaccard Similarity')
+    print(f'\nTask 5: KNN Analysis with Jaccard Similarity')
     data = data[data['year'] >= BASE_YEAR]  # added filter
     knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
                         metric_func=jaccard_similarity_normal, sorted_value='jaccard_similarity')
+
+    # Task 6: KNN Analysis with Weighted Jaccard Similarity
+    print(f"\nTask 6: KNN Analysis with Weighted Jaccard Similarity")
+    base_case = data.loc[BASE_CASE_ID]  # base case
+    second_case = data.loc[SECOND_CASE_ID]  # second case
+    print(
+        f"Comparing all movies to our base case: {
+            base_case['title']} and {second_case['title']}."
+    )
+    # Add two additional filters: stars >= 5 and rating = ['G', 'PG', 'PG-13']
+    data = data[(data["stars"] >= 5) & (
+        data["rating"].isin(["G", "PG", "PG-13"]))]
+    knn_analysis_driver(
+        data,
+        base_case,
+        comparison_type="genres",
+        metric_func=weighted_jaccard_similarity,
+        sorted_value="jaccard_similarity",
+    )
+
+    # Task 7: KNN Analysis with Levenshtein Distance
+    print(f"\nTask 7: KNN Analysis with Levenshtein Distance")
+    # reload data to remove filters
+    data = gt.load_data(data_file, index_col="IMDB_id")
+    base_case = data.loc[BASE_CASE_ID]  # base case
+    print(f"Comparing all movies to our base case: {base_case['title']}.")
+    knn_analysis_driver(
+        data,
+        base_case,
+        comparison_type="title",
+        metric_func=Levenshtein.distance,
+        sorted_value="levenshtein_distance",
+    )
+
+    # Task 8: KNN Analysis with Cosine Similarity
+    print(f'\nTask 8: KNN Analysis with Cosine Similarity')
+    knn_analysis_driver(data, base_case, comparison_type='plot',
+                        metric_func=cosine_similarity_function, sorted_value='cosine_similarity')
+    # Task 9: KNN Analysis with Cosine and Weighted Jaccard
+    print(f'\nTask 9: KNN Analysis with Cosine and Weighted Jaccard')
+    # Add filters
+    data = data[data['year'] >= BASE_YEAR]  # filter by year 1985 and above
+    data = data[(data['stars'] >= 5) & (
+        data['rating'].isin(['G', 'PG', 'PG-13']))]
+    knn_analysis_driver(data, base_case, comparison_type='genres',
+                        metric_func=cosine_and_weighted_jaccard, sorted_value='cosine_and_weighted_jaccard')
 
 
 if __name__ == '__main__':
